4101b9f7c9c32b18150faae655ed98c6c7021de5
vleuschen <10719217@qq.com>
2021-03-14 00:04:11 +0800

    学习Spring For RabbitMQ第一天： 1、初始化项目

---
+++ src/main/java/com/imooc/food/po/OrderDetailPO.java
@@ -0,0 +1,39 @@
+package com.imooc.food.po;
+
+import com.imooc.food.enummeration.OrderStatusEnum;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * order_detal 表对应的Entity
+ */
+@Getter
+@Setter
+@ToString
+public class OrderDetailPO {
+
+    private Integer id;
+
+    private OrderStatusEnum status;
+
+    private String address;
+
+    private Integer accountId;
+
+    private Integer productId;
+
+    private Integer deliverymanId;
+
+    private Integer settlementId;
+
+    private Integer rewardId;
+
+    private BigDecimal price;
+
+    private Date date;
+    
+}
